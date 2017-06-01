71eb7369730415340a712ed49192fb56f2a05277
Rob Blackwell <rob@robblackwell.com>
2017-06-01 08:03:57 +0000

    Updated example

--- examples/example1.py
+++ examples/example1.py
@@ -5,6 +5,8 @@ from echonix import ek60, echogram
 # Displays an echogram of an interesting krill swarm at 38kHz
 
 filename = r'../data/ek60/krill_swarm_20091215/JR230-D20091215-T121917.raw'
-frequency = 38000
+frequency = 120000
 a, r = ek60.raw_to_sv(filename, frequency)
-echogram.show(a, range=r)
+echogram.show(a, max = -50, min = -95, range=r,
+              title='Antarctic krill swarm',
+              cbar_label='Sv (dB)')
