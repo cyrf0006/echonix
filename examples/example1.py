84f0f6ae43436d23e8348089be2e13b79e9baed4
Rob Blackwell <rob@robblackwell.com>
2017-07-05 13:10:03 +0100

    Fixed comment

--- examples/example1.py
+++ examples/example1.py
@@ -2,7 +2,7 @@
 
 from echonix import ek60, echogram
 
-# Displays an echogram of an interesting krill swarm at 38kHz
+# Displays an echogram of an interesting krill swarm at 120kHz
 
 filename = r'../data/ek60/krill_swarm_20091215/JR230-D20091215-T121917.raw'
 frequency = 120000
